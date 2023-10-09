1f1c69929e4af8751d90da1f59dc1bbd49c4ef3b
Stuart Rivero <stuart.rivero.consultant@springernature.com>
2023-10-09 09:55:03 +0100

    Tidy up imports

--- src/test/java/com/example/demolocationapibff/controller/DistanceControllerTest.java
+++ src/test/java/com/example/demolocationapibff/controller/DistanceControllerTest.java
@@ -1,19 +1,15 @@
 package com.example.demolocationapibff.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.example.demolocationapibff.domain.PostcodeValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
